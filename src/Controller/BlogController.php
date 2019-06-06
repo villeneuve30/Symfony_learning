4f48f739c3f40e6cce4e552f2caf1e090b94b7ab
gvi <gvi@emakina.fr>
2019-06-06 10:33:48 +0200

    Création d'un article / Form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,16 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
     /**
@@ -24,9 +34,46 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
      */
-    public function show(){
-        return $this->render('blog/show.html.twig');
+    public function form(Article $article = null, Request $request, ObjectManager $manager){
+        if(!$article){
+            $article = new Article();
+        }
+       
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted()){
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId()
+        ]);
+    }
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article){
+        return $this->render('blog/show.html.twig', [
+            'article' => $article,
+        ]);
     }
 }
